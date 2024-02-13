25a04673d31087ae381a38d3fde5d7a7f98fd653
Anthony Taylor <ant.taylor2@gmail.com>
2024-02-12 20:44:49 -0500

    updating nginx

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: {
-      host: "localhost",
-      protocol: "ws",
-    },
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5173",
-      },
+  build: {
+    outDir: "build",
+    assetsDir: "assets",
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  
+  server: {
+    watch: {
+        usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+}
 });
